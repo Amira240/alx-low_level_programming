e2814b76fe56c852556d8f4c004ad127945faf60
Amira240 <amira.ayman2408@gmail.com>
2023-02-17 15:27:26 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints a text according number
+ * Return: Always (success)
+ *
+ */
+int main(void)
+{	int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
