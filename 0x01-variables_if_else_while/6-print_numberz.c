7000fb1ba6d0b5c5c09024562f4251f520f2faf4
Amira240 <amira.ayman2408@gmail.com>
2023-02-17 14:44:19 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints a text according number
+ * Return: Always (success)
+ *
+ */
+
+int main(void)
+
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
